7735ba648c8e056c294b68a51a6eaf9be1d66438
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2023-01-10 18:46:57 +0300

    Базовая заготовка проекта

---
+++ src/components/app/App.jsx
@@ -0,0 +1,25 @@
+import AppHeader from '../appHeader/AppHeader';
+import decoration from '../../resources/img/vision.png';
+import RandomChar from '../randomChar/RandomChar';
+import CharList from '../charList/CharList';
+import CharInfo from '../charInfo/CharInfo';
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader/>
+            <main>
+                <RandomChar />
+
+                <div class="char__content">
+                    <CharList />
+                    <CharInfo />
+                </div>
+
+                <img className="bg-decoration" src={decoration} alt="vision"/>
+            </main>
+        </div>
+    )
+}
+
+export default App;
