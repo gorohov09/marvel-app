9c2fa9e7eb316123375555049d7b40fb48c21d47
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2023-01-13 15:10:56 +0300

    Реализация компонента CharList

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,51 +1,70 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
 
-const CharList = () => {
+import './charList.scss';
+
+class CharList extends Component {
+
+    state = {
+        characters: []
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.getCharacters();
+    }
+
+    onCharactersLoaded = (characters) => {
+        const charactersMap = characters.map(char => {
+            const {id, ...charProps} = char;
+            return (
+                <CharListItem 
+                key={id}
+                {...charProps}
+                />
+            );
+        });
+
+        this.setState({
+            characters: charactersMap
+        });
+    }
+
+    getCharacters = () => {
+        this.marvelService
+            .getAllCharacters()
+            .then(this.onCharactersLoaded)
+    }
+    
+    render() {
+
+        return (
+            <div className="char__list">
+                <ul className="char__grid">
+                    {this.state.characters}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+}
+
+const CharListItem = (props) => {
+    const {name, thumbnail} = props;
+    const isImageNotAvailable = thumbnail.includes('image_not_available.jpg');
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss" />
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
+        <>
+            <li className="char__item">
+                <img src={thumbnail} 
+                    alt="abyss" 
+                    style={isImageNotAvailable ? {objectFit: 'fill'} : {}}
+                    />
+                <div className="char__name">{name}</div>
+            </li>
+        </>
     )
 }
 
