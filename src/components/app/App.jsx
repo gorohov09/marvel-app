43a1a3cd46ddfe8a5ca819a2dac7bf158a3f3aa3
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2023-01-15 01:12:23 +0300

    Использование предохранителей

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,6 +5,7 @@ import decoration from '../../resources/img/vision.png';
 import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
+import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -23,11 +24,16 @@ class App extends Component {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <RandomChar />
+                    <ErrorBoundary>
+                        <RandomChar />
+                    </ErrorBoundary>
+                    
     
                     <div className="char__content">
                         <CharList onCharSelected={this.onCharSelected}/>
-                        <CharInfo charId={this.state.selectedChar}/>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar}/>
+                        </ErrorBoundary>
                     </div>
     
                     <img className="bg-decoration" src={decoration} alt="vision"/>
