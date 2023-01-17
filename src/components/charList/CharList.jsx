0f3dc31f3fcb3d1c71c5e1b9d0126601430e6881
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2023-01-17 17:45:08 +0300

    Использование prop-types

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../../services/MarvelService';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
@@ -112,6 +114,10 @@ const CharListItem = (props) => {
             </li>
         </>
     )
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
