157098547d34135021927fdc6d9e9a5d7c47f49e
gorohov09 <andrej.gorokhoff2017@yandex.ru>
2023-01-26 23:33:59 +0300

    Добавлена возможность возврата на предыдущую страницу

--- src/components/pages/404.jsx
+++ src/components/pages/404.jsx
@@ -1,12 +1,17 @@
+import { useNavigate  } from "react-router-dom";
+
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import { Link } from "react-router-dom";
 
 const Page404 = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <ErrorMessage />
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px'}}>Page doesn't exist</p>
             <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px'}} to="/">Back to main page</Link>
+            <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px'}} onClick={() => navigate(-1)}>Go back</Link>
         </div>
     )
 }
